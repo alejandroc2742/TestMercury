aabd2c84239c2fec57a7902a608965db5d577c82
Alejandro Cespedes <alejandro.cespedes@mercury-tfs.com>
2022-09-12 12:08:12 -0500

    fix test

--- src/test/java/com/mercury/Test/TestApplicationTests.java
+++ src/test/java/com/mercury/Test/TestApplicationTests.java
@@ -3,7 +3,9 @@ package com.mercury.Test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.mercury.Test.com.mercury.Test.TestApplication;
+
+@SpringBootTest(classes = TestApplication.class)
 class TestApplicationTests {
 
 	@Test
